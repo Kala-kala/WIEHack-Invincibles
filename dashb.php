d99efd706558e6540972be747564a672acd3a21c
Kala-kala <kalaunnatikala@gmail.com>
2018-10-04 16:39:56 +0530

    Add files via upload

---
+++ dashb.php
@@ -0,0 +1,169 @@
+
+<!DOCTYPE html>   <?php include 'side.php'; ?>
+         <?php include 'top.php'; ?>
+         <?php include 'connection.php'; ?>
+  
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <link rel="shortcut icon" href="assets/images/favicon_1.ico">
+
+      
+
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+ <link href="assets/css/sweetalert.css" rel="stylesheet" type="text/css">
+        <script src="assets/js/sweetalert.min.js"></script>
+        <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+        <script src="assets/js/modernizr.min.js"></script>
+
+    </head>
+
+    <body class="fixed-left">
+
+      <!-- Begin page -->
+        <div id="wrapper">
+
+       
+            <!-- Top Bar End -->
+
+
+            <!-- ========== Left Sidebar Start ========== -->
+
+            <!-- Left Sidebar End -->
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+
+                     
+
+   <div class="container">
+   
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="a.jpg" width="700" height="1000">
+      </div>
+
+      <div class="item">
+        <img src="b.jpg" width="700" height="1000">
+      </div>
+    
+      <div class="item">
+        <img src="c1.jpg" width="700" height="1000">
+      </div>   <div class="item">
+        <img src="d1.jpg" width="700" height="1000">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+<br><br>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="portlet">
+                                    <div class="portlet-heading bg-custom">
+                                        <h3 class="portlet-title">
+                                            About Us
+                                        </h3>
+                                        <div class="portlet-widgets">
+                                            <a href="javascript:;" data-toggle="reload"><i class="ion-refresh"></i></a>
+                                            <span class="divider"></span>
+                                            <a data-toggle="collapse" data-parent="#accordion1" href="#bg-primary"><i class="ion-minus-round"></i></a>
+                                            <span class="divider"></span>
+                                            <a href="#" data-toggle="remove"><i class="ion-close-round"></i></a>
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div id="bg-primary" class="panel-collapse collapse in">
+                                        <div class="portlet-body">
+                                        <p>
+                                           <center> <h3>Sanguine Home Makers</h3></center></p>
+<h4>Sanguine Home Makers is dealing with strengthening the home makers by providing them a platform in which they can submit their ideas regarding their product and can interact with the leading female entrepreneur in India as well as government officials. Usually , it is observed that the we are not aware about the policies of the government and so this platform will serve as a connecting platform between the one who is new in that field and the one who knows what entrepreneurship is all about.In this way the home makers is inspired to enter and set a benchmark in the field of entrepreneurship in India . In this platform they can also see the upcoming and existing policies of the government in the field of startups .In this way the home makers will emerge as Trailblazers.</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+     
+
+
+                          
+                         
+                             
+
+            </div> <!-- container -->
+                               
+                </div> <!-- content -->
+
+                  <footer class="footer">
+            
+                   <a href="https://www.facebook.com/covertvagabond/?ref=bookmarks"><h4>Made With <img src="d.jpg" height="14px" width="25px" >By-Team Invincibles</h4></a>
+                </footer>
+
+
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+        <script src="assets/js/jquery.nicescroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+    
+    </body>
+</html>
