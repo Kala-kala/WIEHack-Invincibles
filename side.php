10d4b0dd312d29bb04a93252ca2e87f789d07051
Kala-kala <kalaunnatikala@gmail.com>
2018-10-04 20:28:54 +0530

    Add files via upload

---
+++ side.php
@@ -0,0 +1,21 @@
+<div class="left side-menu" style="top:30px;">
+                <div class="sidebar-inner slimscrollleft">
+                    <!--- Divider -->
+                    <div id="sidebar-menu">
+                        <ul>
+
+                          <li class="text-muted menu-title">Navigation</li>
+   <li><a href="form-elements.php">Fill Form</a></li>   
+   <li><a href="showpolicy.php">View Policy</a></li>    
+   
+
+ <li class="has_sub">
+                                
+                               
+                            </li>            
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
